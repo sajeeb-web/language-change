22745e92687d198669ae23f3d8b0a50ad3f52e0a
sajeeb-web <sajeeb@biometrics-bd.com>
2023-01-05 17:40:10 +0600

    ok

---
+++ assest others/index.js
@@ -0,0 +1,9 @@
+// function myFunction() {
+//     location.replace("https://clever--scone--bef364-netlify-app.translate.goog/?_x_tr_sl=auto&_x_tr_tl=bn&_x_tr_hl=en&_x_tr_pto=wapp");
+//     document.getElementById("one").style.display = "none";
+//   }
+// function checked() {
+//     location.replace("https://clever-scone-bef364.netlify.app/");
+//     document.getElementById("two").style.display = "none";
+//   }
+
